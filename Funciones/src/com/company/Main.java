ae44d1f216eb388f5e5e4c5b866596bf5eadcd4a
rpf1980 <united_ssh@outlook.com>
2018-09-28 18:27:19 +0200

    subo carpeta

---
+++ Funciones/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=============================");
+        System.out.println("MENÚ FUNCIONES cSharp to Java");
+        System.out.println("=============================");
+
+    }
+}
